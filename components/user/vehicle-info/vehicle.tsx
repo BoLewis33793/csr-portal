7f5aea6329d2ba5107ce963eee574fac5121e4c2
BoLewis33793 <105173646+BoLewis33793@users.noreply.github.com>
2025-05-24 13:07:44 -0400

    Fixed Vehicle Import Conflict

--- components/user/vehicle-info/vehicle.tsx
+++ components/user/vehicle-info/vehicle.tsx
@@ -1,27 +1,7 @@
-import { Vehicle } from "@/lib/definitions";
-import { useState, useEffect } from "react";
-
 export default function Vehicle({ id }: { id: number }) {
   const status = [
     "Active", "Overdue", "Cancelled", "Trialing", "Expired", "Refunded"
   ];
-
-  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
-
-  useEffect (() => {
-    const fetchVehicles = async () => {
-      try {
-        const res = await fetch(`/api/users/${id}/vehicles`);
-        const data = await res.json();
-        console.log("Fetched vehicles:", data);
-        setVehicles(data);
-      } catch (error) {
-        console.error("Failed to fetch vehicles: ", error);
-      }
-    };
-
-    fetchVehicles();
-  }, [id]);
 
   return (
     <div className="flex flex-col space-y-2 h-full overflow-hidden">
